eb885a4bddf078fd42a31c3b165a4cbb8ad5d845
lucreziasabba <55913152+lucreziasabba@users.noreply.github.com>
2019-11-22 16:07:06 +0100

    Add files via upload

---
+++ Conv_Test.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConversioniLibreria;
+
+namespace ConversioneTests
+{
+    [TestClass]
+    public class TestConversione
+    {
+        [TestMethod]
+        public void Example1Test()
+        {
+            bool r = Conv.Example1();
+            Assert.AreEqual(true, r);
+        }
+        [TestMethod] 
+        public void Example2Test()
+        {
+            bool r = Conv.Example2();
+            Assert.AreEqual(true, r);
+        }
+    }
+}
