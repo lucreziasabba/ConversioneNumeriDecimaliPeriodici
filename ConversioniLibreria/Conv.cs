a91bd7a9584b1bdbf1ca6c92a03cd09486ac35f9
lucreziasabba <55913152+lucreziasabba@users.noreply.github.com>
2019-11-26 15:15:58 +0100

    Add files via upload

---
+++ ConversioniLibreria/Conv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConversioniLibreria
+{
+    public class Conv
+    {
+        public static bool Example1()
+        {
+            double a = 1.0 / 10.0;
+            double prod = a * 10.0;
+            if (prod == 1.0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Example2()
+        {
+            double a = 1.0 / 10.0;
+            double sum = a + a + a + a + a + a + a +a + a + a;
+            if (sum == 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
